70e047d3d6ef508773153b1c35d64b68169a32b4
𝕙𝕒𝕔𝕜𝕝𝕖𝟞𝟝𝟜 <alexpelmenix@gmail.com>
2026-02-15 02:07:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const Nset = document.querySelector(".nav__setting");
+const Nmenu = document.querySelector(".nav__menu");
 const Nperk = document.querySelector(".nav__perks");
+const Nset = document.querySelector(".nav__setting");
 
 const main = document.querySelector(".main");
 const perk = document.querySelector(".perk");
@@ -12,57 +13,160 @@ Nset.onclick = function () {
 };
 
 Nperk.onclick = function () {
- main.style.display = "none";
- set.style.display = "none";
- perk.style.display = "flex";
-}
-
-const mapsData = {
-    left: [
-        "Kamino",
-        "Tatooine Mos Eisley", 
-        "Nabo Hangar Real",
-        "Hoth",
-        "Takodana",
-        "Dearth Star II",
-        "Yavin 4",
-        "Base Starkiller",
-        "Endor",
-        "Kashyyyk",
-        "Jakku Cemeterio",
-        "Bespin"
-    ],
-    right: [
-        "Palacio de Jabba",
-        "Kessel",
-        "Geonosis Tripa",
-        "Ajian Kloss",
-        "Geonosis Dreadnought",
-        "Nabo Ship",
-        "Felucia",
-        "Takidana MC85",
-        "Jakku Ship",
-        "Crait",
-        "Scarif"
-    ]
+  main.style.display = "none";
+  set.style.display = "none";
+  perk.style.display = "flex";
 };
 
-const container = document.getElementById('mapsContainer');
+Nmenu.onclick = function () {
+  set.style.display = "none";
+  perk.style.display = "none";
+  main.style.display = "flex";
+};
 
+const mapsData = {
+  left: [
+    {
+      name: "Kamino X",
+      image:
+        "https://cdn2.unrealengine.com/egs-starwarsbattlefrontiicelebrationedition-dice-g1a-01-1920x1080-87971829e831.jpg",
+    },
+    { name: "Tatooine Mos Eisley", image: "images/img/maps/tatooine_m.png" },
+    { name: "Nabo Hangar Real", image: "images/img/maps/nabo_h.png" },
+    { name: "Hoth", image: "images/img/maps/hoth.png" },
+    { name: "Takodana", image: "images/img/maps/takodana_c.png" },
+    { name: "Dearth Star II", image: "images/img/maps/death_star.png" },
+    { name: "Yavin 4", image: "images/img/maps/yavin.png" },
+    { name: "Base Starkiller", image: "images/img/maps/starkiller.png" },
+    { name: "Endor", image: "images/img/maps/endor.png" },
+    {
+      name: "Kashyyyk X",
+      image:
+        "https://cdn2.unrealengine.com/egs-starwarsbattlefrontiicelebrationedition-dice-g1a-01-1920x1080-87971829e831.jpg",
+    },
+    {
+      name: "Jakku Cemeterio X",
+      image:
+        "https://cdn2.unrealengine.com/egs-starwarsbattlefrontiicelebrationedition-dice-g1a-01-1920x1080-87971829e831.jpg",
+    },
+    {
+      name: "Bespin X",
+      image:
+        "https://cdn2.unrealengine.com/egs-starwarsbattlefrontiicelebrationedition-dice-g1a-01-1920x1080-87971829e831.jpg",
+    },
+  ],
+  right: [
+    {
+      name: "Palacio de Jabba X",
+      image:
+        "https://cdn2.unrealengine.com/egs-starwarsbattlefrontiicelebrationedition-dice-g1a-01-1920x1080-87971829e831.jpg",
+    },
+    {
+      name: "Kessel X",
+      image:
+        "https://cdn2.unrealengine.com/egs-starwarsbattlefrontiicelebrationedition-dice-g1a-01-1920x1080-87971829e831.jpg",
+    },
+    { name: "Geonosis Tripa", image: "images/img/maps/geonosis_t.png" },
+    { name: "Ajian Kloss", image: "images/img/maps/ajan_kloss.png" },
+    { name: "Geonosis Dreadnought", image: "images/img/maps/geonosis_d.png" },
+    {
+      name: "Nabo Ship X",
+      image:
+        "https://cdn2.unrealengine.com/egs-starwarsbattlefrontiicelebrationedition-dice-g1a-01-1920x1080-87971829e831.jpg",
+    },
+    { name: "Felucia", image: "images/img/maps/felucia.png" },
+    {
+      name: "Takidana MC85 X",
+      image:
+        "https://cdn2.unrealengine.com/egs-starwarsbattlefrontiicelebrationedition-dice-g1a-01-1920x1080-87971829e831.jpg",
+    },
+    {
+      name: "Jakku Ship X",
+      image:
+        "https://cdn2.unrealengine.com/egs-starwarsbattlefrontiicelebrationedition-dice-g1a-01-1920x1080-87971829e831.jpg",
+    },
+    { name: "Crait", image: "images/img/maps/crait.png" },
+    { name: "Scarif", image: "images/img/maps/scarif.png" },
+  ],
+};
+
+const container = document.getElementById("mapsContainer");
+const mainImage = document.querySelector(".main_img");
+
+function changeImage(imagePath) {
+  if (mainImage) {
+    mainImage.src = imagePath;
+    mainImage.style.opacity = "0.5";
+    setTimeout(() => {
+      mainImage.style.opacity = "1";
+    }, 200);
+  }
+}
+
+const leftMaps = mapsData.left
+  .map(
+    (map) =>
+      `<div class="main_map" onclick="changeImage('${map.image}')">${map.name}</div>`
+  )
+  .join("");
+
+// Рендерим правую колонку
+const rightMaps = mapsData.right
+  .map(
+    (map) =>
+      `<div class="main_map" onclick="changeImage('${map.image}')">${map.name}</div>`
+  )
+  .join("");
+
+// Вставляем все в контейнер
 container.innerHTML = `
     <div class="maps_left">
-        ${mapsData.left.map(map => `<div class="main_map">${map}</div>`).join('')}
+        ${leftMaps}
     </div>
     <div class="maps_right">
-        ${mapsData.right.map(map => `<div class="main_map">${map}</div>`).join('')}
+        ${rightMaps}
     </div>
 `;
 
+document.querySelector(".set_dark").onclick = function () {
+  document.body.className = "DarkTheme";
+};
+document.querySelector(".set_light").onclick = function () {
+  document.body.className = "LightTheme";
+};
+
+document.querySelector(".set_dark").onclick = function () {
+  // Добавляем затемнение
+  document.body.style.transition = "opacity 0.3s ease";
+  document.body.style.opacity = "0.1";
+
+  setTimeout(() => {
+    document.body.className = "DarkTheme";
+    document.body.style.opacity = "1";
+  }, 300);
+};
+
+document.querySelector(".set_light").onclick = function () {
+  document.body.style.transition = "opacity 0.3s ease";
+  document.body.style.opacity = "0.1";
+
+  setTimeout(() => {
+    document.body.className = "LightTheme";
+    document.body.style.opacity = "1";
+  }, 300);
+};
+
+document.querySelector(".set_call").onclick = function () {
+  const audio = new Audio("audio/call.mp3");
+  audio.currentTime = 0;
+  audio.play();
+};
+
 document.querySelector(".discord").onclick = function () {
- window.open("https://discord.gg/W2JNJMah");
+  window.open("https://discord.gg/W2JNJMah");
 };
 document.querySelector(".nexus").onclick = function () {
- window.open(
-   "https://www.nexusmods.com/games/starwarsbattlefront22017/mods?timeRange=allTime"
- );
+  window.open(
+    "https://www.nexusmods.com/games/starwarsbattlefront22017/mods?timeRange=allTime"
+  );
 };
